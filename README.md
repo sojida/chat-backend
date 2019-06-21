4d5e04173fe12c1d6c0b4b4af489f78667a91c3e
Adesoji Daniel <sojidaniels@gmail.com>
2019-06-21 16:08:09 +0100

    feature(setup): host app on firebase

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chat-backend
+
+Firebase App - https://chat-back-10336.firebaseapp.com/
---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const path = require('path');
+const express = require('express');
+const socket = require('socket.io');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to chat back')
+});
+
+app.use('*', (req, res) => {
+    res.send('route not found');
+});
+
+const server = app.listen(5000, () => {
+  console.log('server started at port 5000');
+});
+
+// socket setup
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
+});
+
+exports.app = functions.https.onRequest(app);
