120d493f563f8a2914db2807fe79f265549a9a7f
Adesoji Daniel <sojidaniels@gmail.com>
2019-06-28 15:36:45 +0100

    feature(room): create rooms

--- src/index.js
+++ src/index.js
@@ -10,10 +10,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to chat back')
 });
 
-app.use('*', (req, res) => {
-    res.send('route not found');
-});
-
 const port = process.env.PORT || 5000
 
 const server = app.listen(port, () => {
@@ -23,13 +19,28 @@ const server = app.listen(port, () => {
 // socket setup
 const io = socket(server);
 
+app.use('*', (req, res) => {
+    res.send('route not found');
+});
+
+
 io.on('connection', (socket) => {
   console.log('made socket connection', socket.id);
-  socket.on('chat', (data) => {
-    io.sockets.emit('chat', data);
+
+  socket.on('join', (res) => {
+    console.log(`someone joined ${res.pathName}`)
+    socket.join(res.pathName)
+    io.in(res.pathName).emit('join', res);
+
+    socket.on('chat', (data) => {
+      io.in(res.pathName).emit('chat', data);
+    });
+
+    socket.on('typing', (data) => {
+      socket.to(res.pathName).emit('typing', data);
+    });
   });
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', data);
-  });
+
+
 });
 
